18e62fda51ae574e7480eee8b40ae48b4ed53904
houqiangxie <39187588+houqiangxie@users.noreply.github.com>
2022-01-26 07:26:00 +0000

    Deploying to gh-pages from @ houqiangxie/micro-child-app@e692ceda92e05be6cd45fb1fdcda8afba7adb7c9 🚀

---
+++ js/chunk-2d0b68f8.75cec590.js
@@ -0,0 +1,2 @@
+(window["webpackJsonp_micro-child-app"]=window["webpackJsonp_micro-child-app"]||[]).push([["chunk-2d0b68f8"],{"1e4b":function(c,n,d){"use strict";d.r(n);var e=d("7a23"),i=Object(e["f"])(" 我是index "),t=Object(e["e"])("div",{id:"child"},null,-1),p=[i,t];function r(c,n){return Object(e["p"])(),Object(e["d"])("div",null,p)}var b=d("6b0d"),o=d.n(b);const u={},a=o()(u,[["render",r]]);n["default"]=a}}]);
+//# sourceMappingURL=chunk-2d0b68f8.75cec590.js.map
